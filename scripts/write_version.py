6bd75890ad74caf7a36e029b8d04d9aaf7a974f5
Andrei Pashkin <andrew.pashkin@gmx.co.uk>
2020-09-12 12:38:01 +0300

    Make write_version script able to bump versions.
    The change was dictate by realization that you can't have a Git-tag
    and a file with a version derived from it in the same commit :)
    So the solution is to have a script that would generate a new version
    based on the previous one, written in the VERSION file and then tag
    the commit with the new upgraded version-file.
    
    #15

--- scripts/write_version.py
+++ scripts/write_version.py
@@ -1,3 +1,8 @@
+import argparse
+import os
+import re
+from itertools import zip_longest
+
 import dunamai
 
 
@@ -8,14 +13,76 @@ SEMVER_PATTERN = (
     r'(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?'
     r'(?:\+(?P<revision>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
 )
+VERSION_FILE = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    'VERSION'
+)
+
+
+def from_file(file, pattern=SEMVER_PATTERN):
+    with open(file) as version_file:
+        version_string = version_file.read()
+        match = re.match(pattern, version_string)
+        if not match:
+            raise ValueError
+        match_dict = match.groupdict()
+        return dunamai.Version(
+            base=match_dict['base'],
+            stage=match_dict.get('stage'),
+            commit=match_dict.get('revision')
+        )
 
 
 if __name__ == '__main__':
-    version = dunamai.get_version(
-        'gauge',
-        third_choice=lambda: dunamai.Version.from_any_vcs(
-            pattern=SEMVER_PATTERN
+    parser = argparse.ArgumentParser('write-version')
+    parser.add_argument(
+        '-M',
+        '--upgrade-major',
+        action='store_true',
+        default=False
+    )
+    parser.add_argument(
+        '-m',
+        '--upgrade-minor',
+        action='store_true',
+        default=False
+    )
+    parser.add_argument(
+        '-p',
+        '--upgrade-patch',
+        action='store_true',
+        default=False
+    )
+    arguments = parser.parse_args()
+    if (arguments.upgrade_major or
+        arguments.upgrade_minor or
+        arguments.upgrade_patch):
+        version = dunamai.get_version(
+            'gauge',
+            first_choice=lambda: from_file(file=VERSION_FILE)
         )
-    ).serialize(style=dunamai.Style.SemVer)
+    else:
+        version = dunamai.get_version(
+            'gauge',
+            first_choice=lambda: dunamai.Version.from_any_vcs(
+                pattern=SEMVER_PATTERN
+            )
+        )
+    base = dict(zip_longest(
+        ('major', 'minor', 'patch'),
+        map(int, version.base.split('.')),
+        fillvalue=0
+    ))
+    if arguments.upgrade_major:
+        base['major'] += 1
+    if arguments.upgrade_minor:
+        base['minor'] += 1
+    if arguments.upgrade_patch:
+        base['patch'] += 1
+
+    base_string = f'{base["major"]}.{base["minor"]}.{base["patch"]}'
+    version.base = base_string
+    version_string = version.serialize(style=dunamai.Style.SemVer)
+
     with open('VERSION', 'w') as version_file:
-        version_file.write(version)
+        version_file.write(version_string)
