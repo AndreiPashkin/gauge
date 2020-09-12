0db184605798e1ab9bce9f508c7bfadc7b0a62a4
Andrei Pashkin <andrew.pashkin@gmx.co.uk>
2020-09-12 12:33:40 +0300

    Make SemVer regex pattern be raw-string.
    #15

--- scripts/write_version.py
+++ scripts/write_version.py
@@ -2,11 +2,11 @@ import dunamai
 
 
 SEMVER_PATTERN = (
-    '^(?P<base>(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.'
-    '(?P<patch>0|[1-9]\d*))'
-    '(?:-(?P<stage>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)'
-    '(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?'
-    '(?:\+(?P<revision>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
+    r'^(?P<base>(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.'
+    r'(?P<patch>0|[1-9]\d*))'
+    r'(?:-(?P<stage>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)'
+    r'(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?'
+    r'(?:\+(?P<revision>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
 )
 
 
