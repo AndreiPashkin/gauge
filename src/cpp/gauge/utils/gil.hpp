29730b419ba3a106114348fb184f24959d2afd16
Andrei Pashkin <andrew.pashkin@gmx.co.uk>
2020-09-28 15:18:45 +0300

    Make gauge::detail::GILGuard honor Rule of Five.
    #26

--- src/cpp/gauge/utils/gil.hpp
+++ src/cpp/gauge/utils/gil.hpp
@@ -8,6 +8,10 @@ namespace detail {
 class GILGuard {
 public:
     GILGuard();
+    GILGuard(GILGuard &&) = delete;
+    GILGuard &operator=(GILGuard &&) = delete;
+    GILGuard(const GILGuard &)       = delete;
+    GILGuard &operator=(const GILGuard &) = delete;
     ~GILGuard();
 
 private:
